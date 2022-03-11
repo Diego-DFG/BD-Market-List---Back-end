08deca4a80b0fe097bf7b605e07d986e80367693
Diego Gomes <diegofgomes19@gmail.com>
2022-03-10 22:27:17 -0300

    Alteração nos cabeçalhos da requisição

--- index.js
+++ index.js
@@ -7,13 +7,26 @@ const app = express();
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
-app.use(function(req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "https://diego-dfg.github.io/BD-Market-List/#/"); // update to match the domain you will make the request from
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader("Access-Control-Allow-Methods", ["POST, PATCH, PUT, DELETE, OPTIONS"]);
-    next();
-  });
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://diego-dfg.github.io/BD-Market-List/#/');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+
 
   /*app.use(
     cors({
