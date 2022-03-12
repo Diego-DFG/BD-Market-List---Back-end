dc60b3b40813a959a019228b72b6ab911e01cb34
Diego Gomes <diegofgomes19@gmail.com>
2022-03-12 12:05:21 -0300

    Alteração nas rotas para permissão do CORS

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ const app = express();
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
+
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended: true}));
-app.use(cors());
+app.use(cors({credentials: true, origin: "https://diego-dfg.github.io/BD-Market-List/#/"}));
 app.use('/', rotas);
 
 conexao.connect((erro)=> {
